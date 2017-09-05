3ca1bf8b5d87a7912716fe46b98aca0177f4e0cc
Florin Godard <fgodard@gmail.com>
2017-09-05 16:07:28 +0000

    Make bash wrapper script a bit more portable

--- mqttsend.sh
+++ mqttsend.sh
@@ -3,6 +3,20 @@ ownPath="$(readlink -f "$0")"
 ownFolder="$(dirname "$ownPath")"
 cd $ownFolder
 
+if [ ! -f "bin/activate" ]; then
+	echo "No venv found. Please make one!"
+	exit
+fi
+
 source bin/activate
 
-bin/python3.6 mqttsend.py
+
+if [ -f "bin/python3.6" ]; then
+	bin/python3.6 mqttsend.py
+elif [ -f "bin/python3.5" ]; then
+	bin/python3.5 mqttsend.py
+elif [ -f "bin/python3" ]; then
+	bin/python3 mqttsend.py
+else
+	echo "No python3 binary found."
+fi
