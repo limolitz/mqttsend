28becacbdef3d87474d46d64ad0f77b5063a8922
Florin Godard <florin@wasmitnetzen.de>
2019-08-11 21:08:00 +0000

    Make venv wrapper more generic

--- mqttsend.sh
+++ mqttsend.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 cd $(dirname "${BASH_SOURCE[0]}")
 
-if [ ! -f "bin/activate" ]; then
+if [ -f "bin/activate" ]; then
+    source bin/activate
+    if [ ! -f "bin/python3" ]; then
+        echo "No python3 binary found." >&2
+        exit 1
+    fi
+    python_exec="bin/python3"
+elif [ -f "venv/bin/activate" ]; then
+    source venv/bin/activate
+    if [ ! -f "venv/bin/python3" ]; then
+        echo "No python3 binary found." >&2
+        exit 1
+    fi
+    python_exec="venv/bin/python3"
+else
 	echo "No venv found. Please make one!" >&2
 	exit 1
 fi
 
-source bin/activate
-
-
-if [ -f "bin/python3.6" ]; then
-	bin/python3.6 mqttsend.py >> mqttsend.log 2>&1
-elif [ -f "bin/python3.5" ]; then
-	bin/python3.5 mqttsend.py >> mqttsend.log 2>&1
-elif [ -f "bin/python3" ]; then
-	bin/python3 mqttsend.py >> mqttsend.log  2>&1
-else
-	echo "No python3 binary found."
-fi
+$python_exec mqttsend.py >> mqttsend.log  2>&1
